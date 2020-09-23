190f0e05ad6e8bc6b6ebc18cf1133d50c78391bc
rayendito <ryanditod@gmail.com>
2020-09-23 20:03:22 +0700

    ganti matriks jadi float sama prosedur eselon baris insyaAllah bener :')

--- src/Matriks.java
+++ src/Matriks.java
@@ -3,17 +3,17 @@ import java.io.*;
 
 class Matriks{
 	//Atribut
-	int[][] muatriks;
+	float[][] muatriks;
 
 	//Method
 
 	void bacaMatriks(int baris, int kolom) {
-		this.muatriks = new int[baris][kolom];
+		this.muatriks = new float[baris][kolom];
 		Scanner input = new Scanner (System.in);
 		int i,j;
 		for (i=0; i<baris; i++){
 			for (j=0; j<kolom; j++){
-				this.muatriks[i][j] = input.nextInt();
+				this.muatriks[i][j] = input.nextFloat();
 			}
 		}
 	}
@@ -27,4 +27,35 @@ class Matriks{
 			System.out.println();
 		}
 	}
+	void gauss(int baris, int kolom){
+		int i,j,k,l;
+		float faktor;
+		boolean rowDone=false;
+		for(i=0; i<baris; i++){
+			for(j=0; j<kolom; j++){
+				if (this.muatriks[i][j]!=0 && !(rowDone)){
+					faktor = this.muatriks[i][j];
+					for(k=j; k<kolom; k++){
+						this.muatriks[i][k] = this.muatriks[i][k]/faktor;
+					}
+					for(k=i+1; k<baris; k++){
+						faktor = this.muatriks[k][j];
+						for(l=j; l<kolom; l++){
+							this.muatriks[k][l] -= (faktor*this.muatriks[i][l]);
+						}	
+					}
+					rowDone = true;
+				}
+			}
+			rowDone = false;
+		}
+	}
 }
+
+
+
+
+
+
+
+
