06cb691578b25c54b8eb72e287717505f4015a48
arsa dg <arsaadg891@gmail.com>
2020-09-24 15:34:51 +0700

    rename main sama bin sementara aja

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-class mainMatriks{
+class Main{
 	public static void main(String[] args){
 		//ini adalah scanner :)
 		Scanner input = new Scanner (System.in);
