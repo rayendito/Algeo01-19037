06472e8a2bb60219318c1c49052296462bb3f40f
rayendito <ryanditod@gmail.com>
2020-09-30 00:17:54 +0700

    major update Main

--- src/Main.java
+++ src/Main.java
@@ -6,39 +6,40 @@ class Main{
 		//ini adalah scanner :)
 		Scanner input = new Scanner (System.in);
 
-		//input perintah baca file atau input manual
-		System.out.print("Baca file atau input manual? (file/manual/interpol) : ");
-	  	String bac = input.nextLine();
+		//buat objek
+		Matriks M = new Matriks();
+		boolean isMenu = true;
+		int menu, submenu, brskol;
 
-	  	//jika input manual
-	  	if (bac.equals("manual")){
-			//buat objek
-			Matriks M = new Matriks();
-			boolean isMenu = true;
-			int menu, submenu, brskol;
-			while(isMenu == true){
-				System.out.println("MENU");
-				System.out.println("1. Sistem Persamaan Linear");
-				System.out.println("2. Determinan");
-				System.out.println("3. Matriks balikan");
-				System.out.println("4. Interpolasi Polinom");
-				System.out.println("5. Regresi linier berganda");
-				System.out.println("6. Keluar");
-				System.out.print("Masukkan Menu: ");
-				menu = input.nextInt();
+		while(isMenu == true){
+			//Print Menu
+			System.out.println("MENU");
+			System.out.println("1. Sistem Persamaan Linear");
+			System.out.println("2. Determinan");
+			System.out.println("3. Matriks balikan");
+			System.out.println("4. Interpolasi Polinom");
+			System.out.println("5. Regresi linier berganda");
+			System.out.println("6. Keluar");
+			System.out.print("Masukkan Menu: ");
+			menu = input.nextInt();
 
-				if (menu == 1){
-					System.out.println("1. Metode eliminasi Gauss");
-					System.out.println("2. Metode eliminasi Gauss-Jordan");
-					System.out.println("3. Metode matriks balikan");
-					System.out.println("4. Kaidah Cramer");
-					System.out.print("Masukkan Sub-menu: ");
-					submenu = input.nextInt();
-					if (submenu == 1){
-						//
-					}else if (submenu == 2){
-						//
-					}else if (submenu == 3){
+			if (menu == 1){
+				System.out.println("1. Metode eliminasi Gauss");
+				System.out.println("2. Metode eliminasi Gauss-Jordan");
+				System.out.println("3. Metode matriks balikan");
+				System.out.println("4. Kaidah Cramer");
+				System.out.print("Masukkan Sub-menu: ");
+				submenu = input.nextInt();
+				if (submenu == 1){
+					//
+				}else if (submenu == 2){
+					//
+				}else if (submenu == 3){
+					Scanner ingput = new Scanner(System.in);
+					System.out.print("Manual atau File? (manual/file) ");
+					String choose = ingput.nextLine();
+
+					if (choose.equals("manual")){
 						System.out.print("Masukkan jumlah baris/kolom(n): ");
 						brskol = input.nextInt();
 						M.brs = brskol;
@@ -54,21 +55,47 @@ class Main{
 						}else{
 							System.out.println("Matriks bukan persegi");
 						}
-					}else if (submenu == 4){
+					}
+					else if(choose.equals("file")){
+						M.bacaFileMatriks();
+						if (M.isPersegi()){
+							if (M.detRed() != 0){
+								M.splUsingInverse();
+							}else{
+								System.out.println("Determinan matriks sama dengan 0");
+							}
+						}else{
+							System.out.println("Matriks bukan persegi");
+						}
+					}
+				}else if (submenu == 4){
+					Scanner ingput = new Scanner(System.in);
+					System.out.print("Manual atau File? (manual/file) ");
+					String choose = ingput.nextLine();
+
+					if (choose.equals("manual")){
 						System.out.print("Masukkan jumlah baris: ");
 						M.brs = input.nextInt();
 						System.out.print("Masukkan jumlah kolom: ");
 						M.kol = input.nextInt();
 						M.bacaMatriks();
 						M.cramer();
-						
 					}
-				}else if (menu == 2){
-					System.out.println("1. Metode reduksi baris");
-					System.out.println("2. Metode ekspansi kofaktor");
-					System.out.print("Masukkan Sub-menu: ");
-					submenu = input.nextInt();
-					if (submenu == 1){
+					else if(choose.equals("file")){
+						M.bacaFileMatriks();
+						M.cramer();
+					}
+				}
+			}else if (menu == 2){
+				System.out.println("1. Metode reduksi baris");
+				System.out.println("2. Metode ekspansi kofaktor");
+				System.out.print("Masukkan Sub-menu: ");
+				submenu = input.nextInt();
+				if (submenu == 1){
+					Scanner ingput = new Scanner(System.in);
+					System.out.print("Manual atau File? (manual/file)");
+					String choose = ingput.nextLine();
+					if (choose.equals("manual")){
 						System.out.print("Masukkan jumlah baris/kolom(n): ");
 						brskol = input.nextInt();
 						M.brs = brskol;
@@ -80,7 +107,21 @@ class Main{
 						}else{
 							System.out.println("Matriks bukan persegi");
 						}
-					}else if (submenu == 2){
+					}
+					else if(choose.equals("file")){
+						M.bacaFileMatriks();
+						if(M.isPersegi()){
+							System.out.println("Determinan matriks menggunakan metode reduksi baris adalah " + M.detRed());
+						}else{
+							System.out.println("Matriks bukan persegi");
+						}
+					}
+					
+				}else if (submenu == 2){
+					Scanner ingput = new Scanner(System.in);
+					System.out.print("Manual atau File? (manual/file) ");
+					String choose = ingput.nextLine();
+					if (choose.equals("manual")){
 						System.out.print("Masukkan jumlah baris/kolom(n): ");
 						brskol = input.nextInt();
 						M.brs = brskol;
@@ -93,14 +134,44 @@ class Main{
 							System.out.println("Matriks bukan persegi");
 						}
 					}
-				}else if (menu == 3){
-					System.out.println("1. Metode opo iki to");
-					System.out.println("2. Metode adjoin");
-					System.out.print("Masukkan Sub-menu: ");
-					submenu = input.nextInt();
-					if (submenu == 1){
-						
-					}else if (submenu == 2){
+					else if(choose.equals("file")){
+						M.bacaFileMatriks();
+						if(M.isPersegi()){
+							System.out.println("Determinan matriks menggunakan ekspansi kofaktor adalah " + M.detEx());
+						}else{
+							System.out.println("Matriks bukan persegi");
+						}
+					}
+				}
+			}else if (menu == 3){
+				System.out.println("1. Gauss-Jordan");
+				System.out.println("2. Metode adjoin");
+				System.out.print("Masukkan Sub-menu: ");
+				submenu = input.nextInt();
+				if (submenu == 1){
+					Scanner ingput = new Scanner(System.in);
+					System.out.print("Manual atau File? (manual/file)");
+					String choose = ingput.nextLine();
+					if (choose.equals("manual")){
+						System.out.print("Masukkan ukuran matriks: ");
+						M.brs = input.nextInt();
+						M.kol = M.brs;
+						M.bacaMatriks();
+						M.balikanGJordan();
+						M.tulisMatriks();
+					}
+					else if(choose.equals("file")){
+						M.bacaFileMatriks();
+						M.balikanGJordan();
+						M.tulisMatriks();
+					}
+					
+				}else if (submenu == 2){
+
+					Scanner ingput = new Scanner(System.in);
+					System.out.print("Manual atau File? (manual/file)");
+					String choose = ingput.nextLine();
+					if (choose.equals("manual")){
 						System.out.print("Masukkan jumlah baris/kolom(n): ");
 						brskol = input.nextInt();
 						M.brs = brskol;
@@ -118,58 +189,57 @@ class Main{
 							System.out.println("Matriks bukan persegi");
 						}
 					}
-				}else if (menu == 4){
-					//monggo to
-				}else if (menu == 5){
-					//mikir cok
-				}else if (menu == 6){
-					isMenu = false;
+					else if(choose.equals("file")){
+						M.bacaFileMatriks();
+						if(M.isPersegi()){
+							if (M.detRed() != 0){
+								M.inverseAdj();
+								M.tulisMatriks();
+							}else{
+								System.out.println("Determinan matriks sama dengan 0");
+							}
+						}else{
+							System.out.println("Matriks bukan persegi");
+						}
+					}
 				}
+			}else if (menu == 4){
+				Scanner ingput = new Scanner(System.in);
+				System.out.print("Manual atau File? (manual/file)");
+				String choose = ingput.nextLine();
+
+				if (choose.equals("manual")){
+					System.out.print("Masukkan jumlah n : ");
+					int barees = input.nextInt();
+					M.brs = barees;
+					M.kol = 2;
+					M.bacaMatriks();
+					M.interpolasi();
+				}
+				else if(choose.equals("file")){
+					M.bacaFileMatriks();
+					M.interpolasi();
+				}
+				
+			}else if (menu == 5){
+				//mikir cok
+			}else if (menu == 6){
+				isMenu = false;
 			}
-	  		
-			// //input baris dan kolom
-			// System.out.print("Masukkan jumlah baris: ");
-			// M.brs = input.nextInt();
-
-			// System.out.print("Masukkan jumlah kolom: ");
-			// M.kol = input.nextInt();
-
-			// //panggil prosedur bacaMatriks
-			// M.bacaMatriks();
-
-			// M.tulisMatriks();
-
-			// // tes determinan
-			// System.out.println("determinannya: " + M.detRed());
-			
-			// //tes gauss
-			// M.gauss();
-			// M.gaussJordan();
-			// //panggil prosedur tulisMatriks setelah dijadikan eselon baris
-			// M.tulisMatriks();
-			// M.solusi();
-			
-			// //tes apakah matriks persegi
-			// if(M.isPersegi()) {
-			// 	System.out.println("matriks persegi");
-			// }
-			// else System.out.println("matriks tidak persegi");
-			
-			// //tes cramer
-			// System.out.println("Hasil tes Cramer : ");
-			// M.cramer();
-			
-	  	}
-	  	//jika input dengan file
-		else if (bac.equals("file")){
-	  		System.out.print("Nama file : ");
-	  		String namaFile = input.nextLine();
-
-	  		System.out.println("terus yoopo iki hayo aku ga ngerti wkwk");
-	  	}
-	  	else if (bac.equals("interpol")){
-	  		Matriks M = new Matriks();
-	  		M.interpolasi();
-	  	}
+		}
 	}
 }
+
+
+/*
+Scanner ingput = new Scanner(System.in);
+System.out.print("Manual atau File? (manual/file) ");
+String choose = ingput.nextLine();
+
+if (choose.equals("manual")){
+
+}
+else if(choose.equals("file")){
+
+}
+*/
